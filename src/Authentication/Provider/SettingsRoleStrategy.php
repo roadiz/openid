f0dc9d610367ea3dd63670b38e2eb5097de2d090
roadiz-ci <ambroise+ci@roadiz.io>
2025-02-10 10:57:08 +0000

    chore: Bumped

--- src/Authentication/Provider/SettingsRoleStrategy.php
+++ src/Authentication/Provider/SettingsRoleStrategy.php
@@ -10,9 +10,14 @@ class SettingsRoleStrategy implements JwtRoleStrategy
 {
     public const SETTING_NAME = 'openid_default_roles';
 
-    public function __construct(
-        protected readonly ParameterBag $settingsBag,
-    ) {
+    protected ParameterBag $settingsBag;
+
+    /**
+     * @param ParameterBag $settingsBag
+     */
+    public function __construct(ParameterBag $settingsBag)
+    {
+        $this->settingsBag = $settingsBag;
     }
 
     public function supports(): bool
@@ -26,7 +31,6 @@ class SettingsRoleStrategy implements JwtRoleStrategy
         if (!is_string($settings)) {
             return null;
         }
-
         return array_map(function ($role) {
             return trim($role);
         }, explode(',', $settings));
--- src/Exception/DiscoveryNotAvailableException.php
+++ src/Exception/DiscoveryNotAvailableException.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\OpenId\Exception;
 
+use Throwable;
+
 final class DiscoveryNotAvailableException extends \RuntimeException
 {
-    public function __construct(
-        string $message = 'OpenID discovery is not configured',
-        int $code = 0,
-        ?\Throwable $previous = null,
-    ) {
+    /**
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = 'OpenID discovery is not configured', $code = 0, Throwable $previous = null)
+    {
         parent::__construct($message, $code, $previous);
     }
 }
