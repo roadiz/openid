f84e7be7498103166ef6f14756c73c4ba24166aa
roadiz-ci <ambroise+ci@roadiz.io>
2025-07-01 07:42:06 +0000

    chore: Bumped

--- src/Discovery.php
+++ src/Discovery.php
@@ -7,16 +7,11 @@ namespace RZ\Roadiz\OpenId;
 use CoderCat\JWKToPEM\Exception\Base64DecodeException;
 use CoderCat\JWKToPEM\Exception\JWKConverterException;
 use CoderCat\JWKToPEM\JWKConverter;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\RequestException;
 use Psr\Cache\CacheItemPoolInterface;
-use Psr\Cache\InvalidArgumentException;
-use Psr\Log\LoggerInterface;
 use RZ\Roadiz\Bag\LazyParameterBag;
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 /**
  * @package RZ\Roadiz\OpenId
@@ -25,15 +20,20 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class Discovery extends LazyParameterBag
 {
     public const CACHE_KEY = 'rz_openid_discovery_parameters';
+
+    protected string $discoveryUri;
+    protected CacheItemPoolInterface $cacheAdapter;
     protected ?array $jwksData = null;
 
-    public function __construct(
-        protected readonly string $discoveryUri,
-        protected readonly CacheItemPoolInterface $cacheAdapter,
-        protected readonly HttpClientInterface $client,
-        protected readonly LoggerInterface $logger,
-    ) {
+    /**
+     * @param string  $discoveryUri
+     * @param CacheItemPoolInterface $cacheAdapter
+     */
+    public function __construct(string $discoveryUri, CacheItemPoolInterface $cacheAdapter)
+    {
         parent::__construct();
+        $this->discoveryUri = $discoveryUri;
+        $this->cacheAdapter = $cacheAdapter;
     }
 
     public function isValid(): bool
@@ -49,19 +49,16 @@ class Discovery extends LazyParameterBag
             $parameters = $cacheItem->get();
         } else {
             try {
-                $client = $this->client->withOptions([
-                    'timeout' => 2.0,
+                $client = new Client([
+                    // You can set any number of default request options.
+                    'timeout'  => 2.0,
                 ]);
-                $response = $client->request('GET', $this->discoveryUri);
+                $response = $client->get($this->discoveryUri);
                 /** @var array $parameters */
-                $parameters = \json_decode(json: $response->getContent(), associative: true, flags: JSON_THROW_ON_ERROR);
+                $parameters = \json_decode($response->getBody()->getContents(), true);
                 $cacheItem->set($parameters);
                 $this->cacheAdapter->save($cacheItem);
-            } catch (ExceptionInterface $exception) {
-                $this->logger->warning('Cannot fetch OpenID discovery parameters: ' . $exception->getMessage());
-                return;
-            } catch (\JsonException $exception) {
-                $this->logger->warning('Cannot fetch OpenID discovery parameters: ' . $exception->getMessage());
+            } catch (RequestException $exception) {
                 return;
             }
         }
@@ -84,13 +81,8 @@ class Discovery extends LazyParameterBag
     /**
      * @return array<string>|null
      * @throws Base64DecodeException
-     * @throws ClientExceptionInterface
-     * @throws InvalidArgumentException
      * @throws JWKConverterException
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
-     * @throws \JsonException
+     * @throws GuzzleException
      * @see https://auth0.com/docs/tokens/json-web-tokens/json-web-key-sets
      */
     public function getPems(): ?array
@@ -105,12 +97,7 @@ class Discovery extends LazyParameterBag
 
     /**
      * @return array|null
-     * @throws ClientExceptionInterface
-     * @throws InvalidArgumentException
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
-     * @throws \JsonException
+     * @throws GuzzleException
      */
     protected function getJwksData(): ?array
     {
@@ -128,11 +115,12 @@ class Discovery extends LazyParameterBag
                     $this->jwksData = null;
                 }
             } else {
-                $client = $this->client->withOptions([
-                    'timeout' => 2.0,
+                $client = new Client([
+                    // You can set any number of default request options.
+                    'timeout'  => 3.0,
                 ]);
-                $response = $client->request('GET', $jwksUri);
-                $data = \json_decode(json: $response->getContent(), associative: true, flags: JSON_THROW_ON_ERROR);
+                $response = $client->get($jwksUri);
+                $data = \json_decode($response->getBody()->getContents(), true);
                 if (is_array($data)) {
                     $this->jwksData = $data;
                 } else {
