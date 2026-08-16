25160ab59779088b558ccb7c38f3440f7262163b
roadiz-ci <ambroise+ci@roadiz.io>
2026-08-16 13:40:43 +0000

    chore: bumped

--- src/Authentication/OpenIdAuthenticator.php
+++ src/Authentication/OpenIdAuthenticator.php
@@ -6,13 +6,13 @@ namespace RZ\Roadiz\OpenId\Authentication;
 
 use Lcobucci\JWT\Token\Plain;
 use Lcobucci\JWT\Validation\RequiredConstraintsViolated;
+use RZ\Roadiz\JWT\JwtConfigurationFactory;
 use RZ\Roadiz\OpenId\Authentication\Provider\JwtRoleStrategy;
 use RZ\Roadiz\OpenId\Discovery;
 use RZ\Roadiz\OpenId\Exception\DiscoveryNotAvailableException;
 use RZ\Roadiz\OpenId\Exception\OpenIdAuthenticationException;
 use RZ\Roadiz\OpenId\Exception\OpenIdConfigurationException;
 use RZ\Roadiz\OpenId\OAuth2LinkGenerator;
-use RZ\Roadiz\OpenId\OpenIdJwtConfigurationFactory;
 use RZ\Roadiz\OpenId\User\OpenIdAccount;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -44,7 +44,7 @@ final class OpenIdAuthenticator extends AbstractAuthenticator
         private readonly HttpUtils $httpUtils,
         private readonly ?Discovery $discovery,
         private readonly JwtRoleStrategy $roleStrategy,
-        private readonly OpenIdJwtConfigurationFactory $jwtConfigurationFactory,
+        private readonly JwtConfigurationFactory $jwtConfigurationFactory,
         private readonly UrlGeneratorInterface $urlGenerator,
         private readonly CsrfTokenManagerInterface $csrfTokenManager,
         HttpClientInterface $client,
@@ -56,34 +56,28 @@ final class OpenIdAuthenticator extends AbstractAuthenticator
         private readonly bool $requiresLocalUsers,
         private readonly string $usernameClaim = 'email',
         private readonly string $targetPathParameter = '_target_path',
-        private readonly array $defaultRoles = []
+        private readonly array $defaultRoles = [],
     ) {
         $this->client = $client->withOptions([
             // You can set any number of default request options.
-            'timeout'  => 2.0,
+            'timeout' => 2.0,
         ]);
     }
 
-    /**
-     * @inheritDoc
-     */
     public function supports(Request $request): ?bool
     {
-        return null !== $this->discovery &&
-            $this->discovery->isValid() &&
-            $this->httpUtils->checkRequestPath($request, $this->returnPath) &&
-            $request->query->has('state') &&
-            ($request->query->has('code') || $request->query->has('error'));
+        return null !== $this->discovery
+            && $this->discovery->isValid()
+            && $this->httpUtils->checkRequestPath($request, $this->returnPath)
+            && $request->query->has('state')
+            && ($request->query->has('code') || $request->query->has('error'));
     }
 
-    /**
-     * @inheritDoc
-     */
     public function authenticate(Request $request): Passport
     {
         if (
-            null !== $request->query->get('error') &&
-            null !== $request->query->get('error_description')
+            null !== $request->query->get('error')
+            && null !== $request->query->get('error_description')
         ) {
             throw new AuthenticationException((string) $request->query->get('error_description'));
         }
@@ -129,7 +123,7 @@ final class OpenIdAuthenticator extends AbstractAuthenticator
 
         try {
             $tokenEndpoint = $this->discovery->get('token_endpoint');
-            $redirectUri = $request->getSchemeAndHttpHost() . $request->getBaseUrl() . $request->getPathInfo();
+            $redirectUri = $request->getSchemeAndHttpHost().$request->getBaseUrl().$request->getPathInfo();
 
             /*
              * Redirect URI should always use SSL
@@ -147,8 +141,8 @@ final class OpenIdAuthenticator extends AbstractAuthenticator
                     'client_id' => $this->oauthClientId ?? '',
                     'client_secret' => $this->oauthClientSecret ?? '',
                     'redirect_uri' => $redirectUri,
-                    'grant_type' => 'authorization_code'
-                ]
+                    'grant_type' => 'authorization_code',
+                ],
             ]);
             /** @var array $jsonResponse */
             $jsonResponse = \json_decode(json: $response->getContent(), associative: true, flags: JSON_THROW_ON_ERROR);
@@ -158,32 +152,29 @@ final class OpenIdAuthenticator extends AbstractAuthenticator
             $errorTitle = $jsonResponse['error'] ?? $e->getMessage();
             $errorDescription = $jsonResponse['error_description'] ?? '';
 
-            throw new OpenIdAuthenticationException(
-                $errorTitle . ': ' . $errorDescription,
-                $e->getCode(),
-                $e
-            );
+            throw new OpenIdAuthenticationException($errorTitle.': '.$errorDescription, $e->getCode(), $e);
         } catch (ExceptionInterface $e) {
-            throw new OpenIdAuthenticationException(
-                $e->getMessage(),
-                $e->getCode(),
-                $e
-            );
+            throw new OpenIdAuthenticationException($e->getMessage(), $e->getCode(), $e);
         }
 
-        if (empty($jsonResponse['id_token'])) {
+        if (!\is_string($jsonResponse['id_token']) || empty($jsonResponse['id_token'])) {
             throw new OpenIdAuthenticationException('JWT is missing from response.');
         }
 
-        $jwt = $this->jwtConfigurationFactory
-            ->create()
-            ->parser()
-            ->parse((string) $jsonResponse['id_token']);
+        if (!\is_string($this->usernameClaim) || empty($this->usernameClaim)) {
+            throw new OpenIdAuthenticationException('Username claim is not a valid string.');
+        }
 
-        if (!($jwt instanceof Plain)) {
-            throw new OpenIdAuthenticationException(
-                'JWT token must be instance of ' . Plain::class
-            );
+        $configuration = $this->jwtConfigurationFactory->create();
+
+        if (null === $configuration) {
+            throw new OpenIdAuthenticationException('No JWT configuration available.');
+        }
+
+        $jwt = $configuration->parser()->parse($jsonResponse['id_token']);
+
+        if (!$jwt instanceof Plain) {
+            throw new OpenIdAuthenticationException('JWT token must be instance of '.Plain::class);
         }
 
         /*
@@ -205,16 +196,12 @@ final class OpenIdAuthenticator extends AbstractAuthenticator
         }
 
         if (!$jwt->claims()->has($this->usernameClaim)) {
-            throw new OpenIdAuthenticationException(
-                'JWT does not contain “' . $this->usernameClaim . '” claim.'
-            );
+            throw new OpenIdAuthenticationException('JWT does not contain “'.$this->usernameClaim.'” claim.');
         }
 
         $username = $jwt->claims()->get($this->usernameClaim);
         if (!\is_string($username) || empty($username)) {
-            throw new OpenIdAuthenticationException(
-                'JWT “' . $this->usernameClaim . '” claim is not valid.'
-            );
+            throw new OpenIdAuthenticationException('JWT “'.$this->usernameClaim.'” claim is not valid.');
         }
 
         /*
@@ -223,6 +210,9 @@ final class OpenIdAuthenticator extends AbstractAuthenticator
         $customCredentials = new CustomCredentials(
             function (Plain $jwt) {
                 $configuration = $this->jwtConfigurationFactory->create();
+                if (null === $configuration) {
+                    throw new OpenIdAuthenticationException('No JWT configuration available.');
+                }
                 $constraints = $configuration->validationConstraints();
 
                 try {
@@ -230,6 +220,7 @@ final class OpenIdAuthenticator extends AbstractAuthenticator
                 } catch (RequiredConstraintsViolated $e) {
                     throw new OpenIdAuthenticationException($e->getMessage(), 0, $e);
                 }
+
                 return true;
             },
             $jwt
@@ -269,6 +260,7 @@ final class OpenIdAuthenticator extends AbstractAuthenticator
         if ($this->roleStrategy->supports()) {
             $roles = array_merge($roles, $this->roleStrategy->getRoles() ?? []);
         }
+
         return new OpenIdAccount(
             $identity,
             array_unique($roles),
@@ -276,9 +268,6 @@ final class OpenIdAuthenticator extends AbstractAuthenticator
         );
     }
 
-    /**
-     * @inheritDoc
-     */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
@@ -288,9 +277,6 @@ final class OpenIdAuthenticator extends AbstractAuthenticator
         return new RedirectResponse($this->urlGenerator->generate($this->defaultRoute));
     }
 
-    /**
-     * @inheritDoc
-     */
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
         if ($request->hasSession()) {
